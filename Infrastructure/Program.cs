0d641092630bd206a62db2ef4d73de8616fb68a7
LemonPotion <140953841+LemonPotion@users.noreply.github.com>
2024-06-24 17:53:33 +0300

    Dockerfile update;

--- Infrastructure/Program.cs
+++ Infrastructure/Program.cs
@@ -2,43 +2,49 @@ using Infrastructure.Interfaces;
 using Infrastructure.Settings;
 using Refit;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-builder.Services.AddCors(options =>
+namespace Infrastructure
 {
-    options.AddPolicy("AllowAll",
-        builder =>
+    public class Program
+    {
+        public static void Main(string[] args)
         {
-            builder.AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader();
-        });
-});
+            var builder = WebApplication.CreateBuilder(args);
+            
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddControllers();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
 
-builder.Services.Configure<DiscordSettings>(builder.Configuration.GetSection(nameof(DiscordSettings)));
-var discordSettings =builder.Configuration.GetSection(nameof(DiscordSettings)).Get<DiscordSettings>();
+            builder.Services.Configure<DiscordSettings>(builder.Configuration.GetSection(nameof(DiscordSettings)));
+            var discordSettings =builder.Configuration.GetSection(nameof(DiscordSettings)).Get<DiscordSettings>();
+            
+            builder.Services.AddSingleton(RestService.For<IAuthorization>(discordSettings.ApiEndpoint));
+            builder.Services.AddSingleton(RestService.For<IGuildApi>(discordSettings.ApiEndpoint));
+            builder.Services.AddSingleton(RestService.For<IChannelApi>(discordSettings.ApiEndpoint));
+            
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+
+            app.MapControllers();
 
 
-builder.Services.AddSingleton(RestService.For<IAuthorization>(discordSettings.ApiEndpoint));
-builder.Services.AddSingleton(RestService.For<IGuildApi>(discordSettings.ApiEndpoint));
-builder.Services.AddSingleton(RestService.For<IChannelApi>(discordSettings.ApiEndpoint));
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+            app.Run();
+        }
+    }
 }
-
-app.UseRouting();
-
-app.MapControllers();
-
-
-app.Run();
